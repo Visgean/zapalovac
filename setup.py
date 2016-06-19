503d441e0614418b08d039f317c66d83985e2391
Visgean Skeloru <me@visgean.me>
2016-06-20 00:09:35 +0200

    add brojlej setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='Zapalovac',
+    version="0.0.1",
+    description='Robot co chodi zapalovat svicky na http://www.partezlin.cz/',
+    author='Visgean Skeloru',
+    author_email='me@visgean.me',
+    url='https://github.com/Visgean/zapalovac',
+    packages=[
+      'zapalovac'
+    ],
+    install_requires=[
+        'pyquery'
+    ],
+    license='MIT License',
+    zip_safe=True,
+    keywords='boilerplate package',
+    classifiers=['Packages']
+)
